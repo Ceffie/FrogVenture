7dd99b4f0c363a613777f05aacb733fb794f50b6
Cefas <gondolasaurus@gmail.com>
2024-02-11 00:10:14 +0100

    pewpewpew

--- FrogVenture/Assets/PlayerShooter.cs
+++ FrogVenture/Assets/PlayerShooter.cs
@@ -1,41 +1,100 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
+using static UnityEditorInternal.VersionControl.ListControl;
+using static UnityEngine.UI.Image;
 
 public class PlayerShooter : MonoBehaviour
 {
     public GameObject ObjTongue;
     public GameObject ShootPosition;
 
-    public float TongueSpeed = 1f;
+    private float TongueSpeed = 5f;
+
+    private int Bounces = 0;
+
+    private Quaternion deflectRotation;
+
+    private Vector3 deflectDirection;
+
+    private Vector3 dir;
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey("space"))
+        {
+            ShootTongue();
+        }
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("space"))
-        {
-            ShootTongue();
-        }
+        //rotation
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = 5.23f;
 
-        //Get the Screen positions of the object
-        Vector2 positionOnScreen = Camera.main.WorldToViewportPoint(transform.position);
+        Vector3 objectPos = Camera.main.WorldToScreenPoint(transform.position);
+        mousePos.x = mousePos.x - objectPos.x;
+        mousePos.y = mousePos.y - objectPos.y;
 
-        //Get the Screen position of the mouse
-        Vector2 mouseOnScreen = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition);
-
-        //Get the angle between the points
-        float angle = AngleBetweenTwoPoints(positionOnScreen, mouseOnScreen);
-
-        transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
-    }
-
-    float AngleBetweenTwoPoints(Vector3 a, Vector3 b)
-    {
-        return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
+        float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
     }
 
     private void ShootTongue()
     {
-        Instantiate(ObjTongue, ShootPosition.transform.position, ShootPosition.transform.rotation);
+        Ray2D ray = new Ray2D(transform.position, transform.right);
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.right);
+        Debug.DrawRay(ray.origin, ray.direction * 1000, Color.red);
+
+        // getting tongue tip into rotation and transformation
+        
+
+        if (Bounces == 0)
+        {
+            dir = (ObjTongue.transform.position - new Vector3(hit.point.x, hit.point.y, 0f)).normalized;
+
+            ObjTongue.transform.position -= dir * TongueSpeed * Time.deltaTime;
+
+            float distance = Vector3.Distance(ObjTongue.transform.position, new Vector3(hit.point.x, hit.point.y, ObjTongue.transform.position.z));
+            if (distance < .1f)
+            {
+                Bounces++;
+                dir = -deflectDirection;
+                Debug.Log("BOING " + dir);
+            }
+        }
+
+        if (Bounces == 1)
+        {
+            ObjTongue.transform.position -= dir * TongueSpeed * Time.deltaTime;
+            Debug.Log("Schmovin " + dir);
+            //float distance = Vector3.Distance(ObjTongue.transform.position, new Vector3(hit.point.x, hit.point.y, ObjTongue.transform.position.z));
+            //Debug.Log(distance);
+            //if (distance < .1f)
+            //{
+            //    Bounces++;
+            //    Debug.Log("BOING");
+            //}
+        }
+
+        if (hit.collider)
+        {
+            if (hit.collider.tag == "Wall")
+            {
+                // Get a rotation to go from our ray direction (negative, so coming from the wall),
+                // to the normal of whatever surface we hit.
+                deflectRotation = Quaternion.FromToRotation(-ray.direction, hit.normal);
+
+                // We then take that rotation and apply it to the same normal vector to basically
+                // mirror that angle difference.
+                deflectDirection = deflectRotation * hit.normal;
+
+                Debug.DrawRay(hit.point, deflectDirection *100, Color.magenta);
+            }
+        }
     }
 }
